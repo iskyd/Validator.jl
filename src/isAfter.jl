abc240c7e694a9d2435f9aeaa37e637dd5c2e6c5
iskyd <careddumattia@gmail.com>
2022-02-28 15:38:53 +0100

    default now isAfter

--- src/isAfter.jl
+++ src/isAfter.jl
@@ -2,7 +2,7 @@ export isAfter
 
 using Dates
 
-function isAfter(str::String, date::String)::Bool
+function isAfter(str::String, date::String=Dates.format(Dates.now(), "Y-mm-ddTHH:MM:SS"))::Bool
     comparison = Dates.DateTime(date)
     original = Dates.DateTime(str)
 
--- test/isAfter.jl
+++ test/isAfter.jl
@@ -11,4 +11,10 @@
     @test validators.isAfter("2022-02-28T12:00:00", "2022-02-28T09:00:00") === true
     @test validators.isAfter("2022-02-28T12:00:00", "2022-02-28T12:00:00") === false
     @test validators.isAfter("2022-02-28T12:00:00", "2022-03-01T12:00:00") === false
+
+    # default
+    @test validators.isAfter("2022-02-27") === false
+    @test validators.isAfter("2050-02-28") === true
+    @test validators.isAfter("2022-02-28T12:00:00") === false
+    @test validators.isAfter("2050-02-28T09:00:00") === true
 end
