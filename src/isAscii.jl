824700711005e0e740e53b858924fc5d830f21eb
iskyd <careddumattia@gmail.com>
2022-03-02 10:15:09 +0100

    improve code

--- src/isAscii.jl
+++ src/isAscii.jl
@@ -1,7 +1,7 @@
 export isAscii
 
-isAsciiReg = r"^[\x00-\x7F]*$"
-
 function isAscii(str::String)::Bool
-    return match(isAsciiReg, str) === nothing ? false : true
+    isAsciiReg = r"^[\x00-\x7F]*$"
+
+    return contains(str, isAsciiReg)
 end
