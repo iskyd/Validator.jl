a59798a1a3839ddb3cbcec17eb27506b25fc13fe
iskyd <careddumattia@gmail.com>
2022-03-08 16:24:36 +0100

    abstract string in isethereumaddress

--- src/isethereumaddress.jl
+++ src/isethereumaddress.jl
@@ -1,6 +1,6 @@
 export isethereumaddress
 
-function isethereumaddress(str::String)::Bool
+function isethereumaddress(str::AbstractString)::Bool
     ethReg = r"^(0x)[0-9a-f]{40}$"i
 
     return contains(str, ethReg)
